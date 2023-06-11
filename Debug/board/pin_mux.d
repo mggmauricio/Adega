205faba35b61c96196d9539e1f5fc91eb661c165
Mauricio Godoy <mauriciomgodoy01@gmail.com>
2023-06-11 05:15:33 -0300

    Temperature measure working

---
+++ Debug/board/pin_mux.d
@@ -0,0 +1,31 @@
+board/pin_mux.o board/pin_mux.d: ../board/pin_mux.c \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_common.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/fsl_device_registers.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/core_cm4.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_version.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_compiler.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_gcc.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/system_MK64F12.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12_features.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_common_arm.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_clock.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_common.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_port.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_gpio.h \
+ ../board/pin_mux.h
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_common.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/fsl_device_registers.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/core_cm4.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_version.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_compiler.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_gcc.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/system_MK64F12.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12_features.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_common_arm.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_clock.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_common.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_port.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\drivers/fsl_gpio.h:
+../board/pin_mux.h:
---
+++ Debug/drivers/fsl_ftm.d
@@ -0,0 +1,23 @@
+drivers/fsl_ftm.o drivers/fsl_ftm.d: ../drivers/fsl_ftm.c \
+ ../drivers/fsl_ftm.h ../drivers/fsl_common.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/fsl_device_registers.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/core_cm4.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_version.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_compiler.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_gcc.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/system_MK64F12.h \
+ C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12_features.h \
+ ../drivers/fsl_common_arm.h ../drivers/fsl_clock.h
+../drivers/fsl_ftm.h:
+../drivers/fsl_common.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/fsl_device_registers.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/core_cm4.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_version.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_compiler.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\CMSIS/cmsis_gcc.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/system_MK64F12.h:
+C:\Users\mauri\Documents\MCUXpressoIDE_11.7.1_9221\workspace\MK64F12_Project_Adega\device/MK64F12_features.h:
+../drivers/fsl_common_arm.h:
+../drivers/fsl_clock.h:
